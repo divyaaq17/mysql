4b8dca7629fc613f8daeae08293750fc2379d2ed
divyaaq17 <147229380+divyaaq17@users.noreply.github.com>
2025-10-31 12:49:52 +0530

    Update index.js
    organized code, used routes, insert, update delete queries

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 const express = require('express')
-const mysql = require('mysql2')
+const db = require("./utils/db-connection")
+const studentRoutes = require('./Routes/studentsRoutes')
 const app = express();
 
-const connection =mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'mysql123',
-    database:'testDB'
-})
-
-connection.connect((err)=>{
-    if(err){
-        console.log(err);
-             return;
-    }
-    console.log("Connection has been created");
-
-    const createQuery = `create table students(id INT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(20),
-                        email VARCHAR(20))`
-
-connection.execute(createQuery,(err)=>{
-    if(err){
-    console.log(err);
-        connection.end()
-        return;
-    }
-    console.log("table is created")
-})
-})
+app.use(express.json())
 app.get('/',(req,res)=>
 {
     res.send('Hello World!')
 })
+
+app.use("/students",studentRoutes)
 
 app.listen(4000,(err)=>{
     console.log("Server is running")
