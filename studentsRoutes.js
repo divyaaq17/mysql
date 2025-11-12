826850039df0d4c0d9c6a542e4b44a9b8308144f
divyaaq17 <147229380+divyaaq17@users.noreply.github.com>
2025-11-12 13:12:52 +0530

    Update studentsRoutes.js
    added few lines

--- studentsRoutes.js
+++ studentsRoutes.js
@@ -3,8 +3,10 @@ const studentController = require('../Controllers/studentController')
 const route = express.Router();
 
 route.post('/add',studentController.addEntries)
+route.get('/ShowAll',studentController.showEntries)
+route.get('/ShowStudentbyId/:id',studentController.showStudentbyId)
 route.put('/update/:id',studentController.updateEntry)
-route.put('/delete/:id',studentController.deleteEntry)
+route.delete('/delete/:id',studentController.deleteEntry)
 
 
 module.exports=route;
