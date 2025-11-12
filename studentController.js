8421fb3e30f3bb996e36ab063bd256413307d502
divyaaq17 <147229380+divyaaq17@users.noreply.github.com>
2025-11-12 13:12:24 +0530

    Update studentController.js
    added few functionalities

--- studentController.js
+++ studentController.js
@@ -1,17 +1,17 @@
-const connection = require('../utils/db-connection');
+
 const db = require('../utils/db-connection')
 
 
 const addEntries=(req,res)=>{
 
-    const {email,name} = req.body;
-    const insertQuery='INSERT INTO STUDENTS (email,name) VALUES (?,?)'
+    const {email,name,age} = req.body;
+    const insertQuery='INSERT INTO STUDENTS (email,name,age) VALUES (?,?,?)'
 
-    db.execute(insertQuery,[email,name],(err) =>{
+    db.execute(insertQuery,[email,name,age],(err) =>{
         if(err){
             console.log(err.message)
             res.status(200).send(err.message)
-            db.end();
+            
             return;
         }
         console.log("INSERTED")
@@ -22,13 +22,14 @@ const addEntries=(req,res)=>{
 const updateEntry= (req,res)=>{
     const {id} = req.params;
     const {name} = req.body;
-    const updateQuery='UPDATE STUDENTS set name = ? where id=?'
+    const {email} = req.body;
+    const updateQuery='UPDATE STUDENTS set name = ?, email=? where id=?'
 
-    db.execute(updateQuery,[name,id],(err,result) =>{
+    db.execute(updateQuery,[name,email,id],(err,result) =>{
         if(err){
             console.log(err.message)
             res.status(200).send(err.message)
-            db.end();
+            
             return;
         }
         if(result.affectedRows===0)
@@ -40,6 +41,40 @@ const updateEntry= (req,res)=>{
         console.log("UPDATED")
         res.status(200).send(`Student has been updated`)
     })
+}
+
+const showEntries = (req,res)=>{
+  
+    const selectQuery = `Select * from Students`   
+         db.execute(selectQuery,(err)=>{
+        if(err){
+            console.log(err.message);
+            res.status(200).send(err.message);
+            
+            return;
+            }
+        console.log("RETRIEVE ALL STUDENTS")
+        res.status(200).send("RETRIEVE ALL STUDENTS..")
+
+    })
+
+}
+
+const showStudentbyId = (req,res)=>{
+    const {id} = req.params;
+    const selectQuery = `Select * from Students where id>?`   
+         db.execute(selectQuery,[id],(err)=>{
+        if(err){
+            console.log(err.message);
+            res.status(200).send(err.message);
+            
+            return;
+            }
+        console.log("RETRIEVE THE STUDENT")
+        res.status(200).send("RETRIEVE THE STUDENT..")
+
+    })
+
 }
 
 const deleteEntry= (req,res)=>{
@@ -67,5 +102,7 @@ const deleteEntry= (req,res)=>{
 module.exports={
     addEntries,
     updateEntry,
-    deleteEntry
+    deleteEntry,
+    showEntries,
+    showStudentbyId
 }
