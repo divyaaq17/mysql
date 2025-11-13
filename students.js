82d5bed093c27927d7e93ca9be919a3b9a71ad1a
divyaaq17 <147229380+divyaaq17@users.noreply.github.com>
2025-11-13 13:09:24 +0530

    Add files via upload

---
+++ students.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require("../utils/db-connection")
+
+const Students = sequelize.define(
+  'Students',
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }
+);
+
+module.exports=Students;
